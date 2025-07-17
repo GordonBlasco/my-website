aab38478d1301bd7a1cdadb246012dd3d9c2026e
Gordon Blasco <gordonblasco@gmail.com>
2025-07-16 23:00:21 -0700

    navbar changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        heroicons: ["*"],
+      },
+    }),
+  ],
   adapter: netlify(),
 });
